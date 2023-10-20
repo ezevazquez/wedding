18a99ee4210dd76d406820fc163e306b12c5bf8e
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-20 19:09:43 -0300

    add conunt down

---
+++ app/components/Contador.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import styles from '../styles/Contador.module.css';
+
+interface Props {
+  targetDate: string; // La fecha objetivo en formato ISO (ejemplo: '2024-12-31T23:59:59')
+}
+
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const Contador: React.FC<Props> = ({ targetDate }) => {
+  const [time, setTime] = useState<TimeLeft>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const difference = +new Date(targetDate) - +new Date();
+      let timeLeft: TimeLeft = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        };
+      }
+
+      return timeLeft;
+    };
+
+    const timer = setInterval(() => {
+      setTime(calculateTimeLeft());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [targetDate]);
+
+  return (
+    <div className={styles.contador}>
+      <div className={styles.valueBox}>
+        <div className={styles.value}>{String(time.days)}</div>
+        <div className={styles.time}>días</div>
+      </div>
+      <div className={styles.valueBox}>
+        <div className={styles.value}>{String(time.hours)}</div>
+        <div className={styles.time}>horas</div>
+      </div>
+      <div className={styles.valueBox}>
+        <div className={styles.value}>{String(time.minutes)}</div>
+        <div className={styles.time}>minutos</div>
+      </div>
+      <div className={styles.valueBox}>
+        <div className={styles.value}>{String(time.seconds)}</div>
+        <div className={styles.time}>segundos</div>
+      </div>
+    </div>
+  );
+};
+
+export default Contador;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,20 @@
+'use client'
+
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-
+import Contador from '../components/Contador';
 
 const Index: React.FC = () => {
+  // Reemplaza '2024-12-31T23:59:59' con la fecha objetivo deseada en formato ISO
+  const targetDate = '2024-10-10T20:00:00';
+
   return (
     <div>
       <Navbar />
       <main>
         <Hero />
+        <Contador targetDate={targetDate} />
       </main>
       {/* <Footer /> */}
     </div>
