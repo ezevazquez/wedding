7ce8437d321551ee79e3f5c61bcfe582b60d8252
Eze Vazquez <eze@Ezes-MacBook-Air.local>
2023-10-24 00:20:14 -0300

    update dependancies and fix error running npm run build

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,7 +6,6 @@ import Hero from '../components/Hero';
 import Contador from '../components/Contador';
 
 const Index: React.FC = () => {
-  // Reemplaza '2024-12-31T23:59:59' con la fecha objetivo deseada en formato ISO
   const targetDate = '2024-10-10T20:00:00';
 
   return (
