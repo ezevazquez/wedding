6dbd4dad2c19e7f25707dd1eabcb2f3b733e91c4
Eze Vazquez <eze@Ezes-Air.fibertel.com.ar>
2024-02-13 20:43:38 -0300

    crear contador lindo y centrado

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,10 +1,7 @@
 'use client'
 
 import React from 'react';
-import Hero from '../components/Hero';
 import Contador from '../components/Contador';
-import Contador2 from '../components/Contador2';
-import { Time } from '../types/types';
 
 const Index: React.FC = () => {
   const targetDate = '2024-08-18T17:30:00';
